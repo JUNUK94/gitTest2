97d179fc238316768669ddcbe581bd2fc6e63b21
white <0158sss@naver.com>
2020-02-21 15:57:06 +0900

    abc

--- src/gitTest2/GitTest.java
+++ src/gitTest2/GitTest.java
@@ -5,14 +5,15 @@ public class GitTest {
 	public static void main(String[] args) {
 
 		System.out.println("1123123123221");
-		System.out.println("JPY");/////////sa/df/ds/fds///////
+		System.out.println("YHYHYHY");/////////sa/df/ds/fds///////
 
 System.out.println("1123123123221");
 		
-		//한창현 수정
+		//RRRRRR
 		for(int i=0; i<4; i++) {
-			System.out.println("ㅋㅋ");
+			System.out.println("aa");
 		}
+	
 	}
 
 }
