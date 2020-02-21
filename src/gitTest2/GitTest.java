556901dcfe7bfab424ee663af17e1a2a59861ed7
wowgarcon <herbing@naver.com>
2020-02-21 15:39:46 +0900

    wowgarcon modified

--- src/gitTest2/GitTest.java
+++ src/gitTest2/GitTest.java
@@ -4,6 +4,11 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("2222221");
+		
+		//한창현 수정
+		for(int i=0; i<4; i++) {
+			System.out.println("ㅋ");
+		}
 	}
 
 }
